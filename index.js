333e6047bec80433ede802d3ffbf925c6376951b
Okxu7 <112314598+Okxu7@users.noreply.github.com>
2023-04-08 21:16:52 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-function  sendMail()  {
-    var params = {
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    };
-    console.log(email);
-    
-    console.log(message);
+/* <form>
+  <input type="text" id="email" />
+    </form>
+<script>
 
-   
-   
-    const serviceID = "service_q9xso4n";
-const templateID = "template_6wtcbiq";
+function signUp(event) {
+  event.preventDefault();
+  let input = document.querySelector("#email");
+  console.log(input.value);
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", signUp);
+</script>} */
 
-emailjs.send(serviceID,  templateID,  params)
-    .then(
-        res => {
-    document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
+function sendMail(event) {
+  event.preventDefault();
+  let input = document.querySelector("#email");
+  let input2 = document.querySelector("#message");
+  console.log(input.value);
+  console.log(input2.value);
 
-    console.log(res);
-  alert("Success!"); 
-  })
+  const serviceID = "service_q9xso4n";
+  const templateID = "template_6wtcbiq";
 
+  var params = {
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
 
-.catch ((err)   =>   console.log(err)); 
-   
-    
- }
+  emailjs.send(serviceID, templateID, params).then(function (response) {
+    console.log("SUCCESS!", response.status, response.text);
+  });
 
+  alert("Success!");
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", sendMail);
